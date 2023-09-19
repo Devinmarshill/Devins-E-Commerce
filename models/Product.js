0c69f0ea06dc1a82d42437013454d367950b3004
Devin <devinhill2911@gmail.com>
2023-09-18 22:29:21 -0400

    issues with npm

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,6 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
-
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
@@ -17,10 +16,10 @@ Product.init(
   },
   product_name: {
     type:DataTypes.STRING,
-    allowNull: false,
+    allowNull: false
   },
   price: {
-    tpye:DataTypes.DECIMAL,
+    type: DataTypes.DECIMAL (10 , 2),
     allowNull: false,
     validate: {isDecimal: true}
 
